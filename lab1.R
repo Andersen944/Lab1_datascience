8fe6552bc3f07e3aa2e4d3f2649ac5ec17dc1ca2
Andersen944 <98593328+Andersen944@users.noreply.github.com>
2022-09-16 10:06:16 +0200

    Add files via upload

---
+++ lab1.R
@@ -0,0 +1,24 @@
+## opgave 1
+V <- seq(2,10,by=2)
+
+## opgave 2
+V1<- V[3]<3
+
+##opgave 3
+V2 <- rev(V)/2
+
+##opgave 4
+a <- 5
+e <- 7
+V3 <- 5>7
+
+##opgave 5
+alpha <- round(log(150))
+beta <- 50/10
+
+V4<- alpha==beta
+
+##opgave 6
+library(ggplot2)
+
+ggplot() + geom_point(data = mtcars, aes(x = disp, y = mpg))
